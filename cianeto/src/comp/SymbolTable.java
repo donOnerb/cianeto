17891d43c3af2fdae17c353c91866e50d1fd33b1
Michael Santos <michael-santos@outlook.com.br>
2019-02-05 20:47:40 -0200

    Criada tabelas de simbolos

--- cianeto/src/comp/SymbolTable.java
+++ cianeto/src/comp/SymbolTable.java
@@ -1,6 +1,47 @@
 package comp;
+import java.util.*;
 
 
 public class SymbolTable {
+	 Hashtable globalTable;
+     Hashtable localTable;
+	
+	public SymbolTable() {
+        Hashtable globalTable = new Hashtable();
+        Hashtable localTable  = new Hashtable();
+	}
+    
+    public Object putInGlobal( String key, Object value ) {
+       return globalTable.put(key, value);
+    }
 
+    public Object putInLocal( String key, Object value ) {
+       return localTable.put(key, value);
+    }
+    
+    public Object getInGlobal( String key ) {
+       return globalTable.get(key);
+    }
+    
+    public Object getInLocal( String key ) {
+       return localTable.get(key);
+    }
+    
+    public Object get( String key ) {
+        // returns the object corresponding to the key. 
+        Object result;
+        if ( (result = localTable.get(key)) != null ) {
+              // found local identifier
+            return result;
+        }
+        else {
+              // global identifier, if it is in globalTable
+            return globalTable.get(key);
+        }
+    }
+
+    public void removeLocalIdent() {
+           // remove all local identifiers from the table
+         localTable.clear();
+    }
 }
